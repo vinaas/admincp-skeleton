6f1d765ad3dd32399f52d7ba74aec8c045d634b5
tungptvn <tungpt.hd@gmail.com>
2017-02-16 21:50:56 +0700

    feat(plugin) add select2

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import {Aurelia} from 'aurelia-framework';
-import {Router, RouterConfiguration} from 'aurelia-router';
+import { Aurelia } from 'aurelia-framework';
+import { Router, RouterConfiguration } from 'aurelia-router';
 
 export class App {
   router: Router;
@@ -7,9 +7,10 @@ export class App {
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: './users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' },
+      { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Github Users' },
+      { route: 'child-router', name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' },
+      { route: 'select2-example', name: 'select2-example', moduleId: './select2-ex', nav: true, title: 'Select2 example' },
     ]);
 
     this.router = router;
---
+++ src/select2-ex.ts
@@ -0,0 +1,9 @@
+
+import 'select2';
+import $ from 'jquery';
+export class Select2Example{
+    attached(){
+          $(".js-example-basic-single").select2();
+
+    }
+}
