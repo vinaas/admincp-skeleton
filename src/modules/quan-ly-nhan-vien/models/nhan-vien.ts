dd8dd36241c6ce8b7af770dd3e6be6249658f6a2
tungptvn <tungpt.hd@gmail.com>
2017-03-06 23:07:22 +0700

    feat(modules[nhanvien]) update NhanVien model

--- src/modules/quan-ly-nhan-vien/models/nhan-vien.ts
+++ src/modules/quan-ly-nhan-vien/models/nhan-vien.ts
@@ -1,21 +1,21 @@
-export class NhanVien{
-    public MaNv : number;
-    HoTen :String;
-    ChucVu:String;
-    Email:String;
-    constructor(nhanVien?:any) {
-        if(nhanVien){
-            this.HoTen = nhanVien.HoTen;
-            this.ChucVu = nhanVien.ChucVu;
-            this.Email = nhanVien.Email;
-        }
+export class NhanVien {
+    public MaNv: number;
+    HoTen: String;
+    ChucVu: String;
+    Email: String;
+    constructor(nhanVien: any = { MaNv: 0 }) {
+        this.MaNv = nhanVien.MaNv;
+        this.ChucVu = nhanVien.ChucVu;
+        this.Email = nhanVien.Email;
+        this.HoTen = nhanVien.HoTen;
+
     }
 }
 
 // define validation model
-import {ValidationRules} from 'aurelia-validation';
-ValidationRules  
-  .ensure( (x:NhanVien)=>x.HoTen).required()
-  .ensure(x=>x.ChucVu).required()
-  .ensure(x=>x.Email).required().email()
-  .on(NhanVien);
+import { ValidationRules } from 'aurelia-validation';
+ValidationRules
+    .ensure((x: NhanVien) => x.HoTen).required()
+    .ensure(x => x.ChucVu).required()
+    .ensure(x => x.Email).required().email()
+    .on(NhanVien);
