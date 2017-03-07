787e82e5e655d198b2803a253c159d37a3c6169b
tungptvn <tungpt.hd@gmail.com>
2017-03-07 13:35:31 +0700

    feat(task) publish favicon

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,40 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var runSequence = require('run-sequence');
-gulp.copy=function(src,dest){
-    return gulp.src(src, {base:"."})
-        .pipe(gulp.dest(dest));
+gulp.copy = function (src, dest) {
+  return gulp.src(src, {
+      base: "."
+    })
+    .pipe(gulp.dest(dest));
 };
 
-
-gulp.task('deploy', function(callback) {
-  runSequence(['copy-assets','copy-pages','copy-images'],
-              'push-to-gh-pages',
-              callback);
+gulp.task('deploy', function (callback) {
+  runSequence(['copy-assets', 'copy-pages', 'copy-images', 'copy-favicon'],
+    'push-to-gh-pages',
+    callback);
 });
 
-gulp.task('copy-assets', function() {
-    
-    return gulp.src(['assets/**/*']).pipe(gulp.dest('dist/assets'));
+gulp.task('copy-assets', function () {
+
+  return gulp.src(['assets/**/*']).pipe(gulp.dest('dist/assets'));
 
 });
-gulp.task('copy-pages', function() {
-    
-    return gulp.src(['pages/**/*']).pipe(gulp.dest('dist/pages'));
+gulp.task('copy-pages', function () {
+
+  return gulp.src(['pages/**/*']).pipe(gulp.dest('dist/pages'));
 
 });
-gulp.task('copy-images', function() {
-    
-    return gulp.src(['images/**/*']).pipe(gulp.dest('dist/images'));
+gulp.task('copy-images', function () {
+
+  return gulp.src(['images/**/*']).pipe(gulp.dest('dist/images'));
 
 });
-gulp.task('push-to-gh-pages', function() {
+gulp.task('copy-favicon', function () {
+
+  return gulp.src(['favicon.ico']).pipe(gulp.dest('dist/'));
+
+});
+gulp.task('push-to-gh-pages', function () {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
