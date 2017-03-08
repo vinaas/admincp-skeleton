ba6895c45bbf9999741b62687f951558f228f8f0
tungptvn <tungpt.hd@gmail.com>
2017-03-08 13:55:41 +0700

    feat(*) fix animation nav

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,17 @@ export class App {
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome', auth:true  },
+      { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome', auth: true },
       { route: 'quan-ly-nhan-vien', name: 'quan-ly-nhan-vien', moduleId: 'modules/quan-ly-nhan-vien/index', nav: true, title: 'Quản lý nhân viên' },
     ]);
 
     this.router = router;
 
+  }
+  attached() {
+    var script = document.createElement("script");
+    script.src = "pages/js/scripts.js";
+    script.type = "text/javascript";
+    document.getElementsByTagName("head")[0].appendChild(script);
   }
 }
