081982e15ba0d71fa9fca4643f80052276a8cc15
tungptvn <tungpt.hd@gmail.com>
2017-03-10 15:36:59 +0700

    chore(*) : rename controller to dialogController

--- src/modules/quan-ly-nhan-vien/dialogs/luu-nhan-vien.ts
+++ src/modules/quan-ly-nhan-vien/dialogs/luu-nhan-vien.ts
@@ -6,32 +6,32 @@ import { ValidationControllerFactory, ValidationController } from "aurelia-valid
 @inject(DialogController, ValidationControllerFactory)
 
 export class SaveNhanVien {
-    validationcontroller: ValidationController;
-    constructor(private controller, private controllerFactory) {
-        this.validationcontroller = controllerFactory.createForCurrentScope();
-        this.validationcontroller.addRenderer(new BootstrapFormRenderer());
-    }
-    get getTieuDe() {
-        switch (this.nhanVienDto.MaNv) {
-            case 0:
-                return "Thêm mới nhân viên";
+  validationcontroller: ValidationController;
+  constructor(private dialogcontroller: DialogController, private controllerFactory) {
+    this.validationcontroller = controllerFactory.createForCurrentScope();
+    this.validationcontroller.addRenderer(new BootstrapFormRenderer());
+  }
+  get getTieuDe() {
+    switch (this.nhanVienDto.MaNv) {
+      case 0:
+        return "Thêm mới nhân viên";
 
-            default:
-                return "Cập nhật nhân viên";
-        }
+      default:
+        return "Cập nhật nhân viên";
     }
-    nhanVienDto: NhanVien;
-    activate(dto: NhanVien) {
-        console.log('dto', dto);
-        this.nhanVienDto = dto;
-    }
-    save() {
-        this.validationcontroller.validate().then((result) => {
-            if (result.valid) {
-                this.controller.ok(this.nhanVienDto);
-            }
-        })
+  }
+  nhanVienDto: NhanVien;
+  activate(dto: NhanVien) {
+    console.log('dto', dto);
+    this.nhanVienDto = dto;
+  }
+  save() {
+    this.validationcontroller.validate().then((result) => {
+      if (result.valid) {
+        this.dialogcontroller.ok(this.nhanVienDto);
+      }
+    })
 
-    }
+  }
 
 }
