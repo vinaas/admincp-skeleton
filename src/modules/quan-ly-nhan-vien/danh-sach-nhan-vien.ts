70059db41cd6a4dc4efb47098ec9efb479c55eb8
tungptvn <tungpt.hd@gmail.com>
2017-03-10 15:38:54 +0700

    feat(modules[nhanvien]) add selected nhanviens function

--- src/modules/quan-ly-nhan-vien/danh-sach-nhan-vien.ts
+++ src/modules/quan-ly-nhan-vien/danh-sach-nhan-vien.ts
@@ -19,11 +19,22 @@ export class DanhSachNhanVien {
   private columnApi: ColumnApi;
   private allOfTheData: any = [];
   private columnDefs: any[];
+
+  private selectdRows: NhanVien[] = [];
+
+
   constructor(private quanLyNhanVienService: QuanLyNhanVienServiceInterface, private dialogService) {
     this.columnDefs = [
       {
-        headerName: "Mã", field: "MaNv", filter: 'number'
+        headerName: "Chọn",
+        width: 30,
+        headerCheckboxSelection: true,
+        headerCheckboxSelectionFilteredOnly: true,
+        checkboxSelection: true
       },
+      // {
+      //   headerName: "Mã", field: "MaNv", filter: 'number'
+      // },
       { headerName: "Chức vụ", field: "ChucVu", suppressMenu: false, suppressSorting: true },
       { headerName: "Họ tên", field: "HoTen", filter: 'text', filterParams: { apply: true, newRowsAction: 'keep' }, suppressMenu: false, suppressSorting: true },
       { headerName: "Email", field: "Email", filter: 'text', filterParams: { newRowsAction: 'keep' }, suppressMenu: false, suppressSorting: true },
@@ -32,22 +43,17 @@ export class DanhSachNhanVien {
         suppressMenu: true,
         suppressSorting: true,
         template:
-        `<button type="button" class="btn btn-xs" data-action-type="view" >
-               Xem
-             </button>
-          <button type="button" class="btn btn-xs" data-action-type="edit" >
-               Sửa
-          </button>
-          <button type="button" class="btn btn-xs" data-action-type="remove" >
-               Xóa
-          </button>`
+        `<button type="button" class="btn btn-default btn-xs" data-action-type="edit">
+          <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Chi tiết
+        </button>
+        `
       }
     ];
     this.gridOptions = {
       enableSorting: true,
       enableFilter: true,
       enableColResize: true,
-      paginationPageSize: 100,
+      paginationPageSize: 20,
       columnDefs: this.columnDefs,
       rowModelType: 'pagination',
       rowSelection: 'multiple',
@@ -64,8 +70,32 @@ export class DanhSachNhanVien {
     })
 
   }
+
   onReady() {
     this.createNewDatasource();
+  }
+  createNewDatasource() {
+    if (!this.allOfTheData) {
+      return;
+    }
+    var dataSource = {
+      getRows: (params) => {
+       
+        this.quanLyNhanVienService.GetNhanViens().then(res => {
+          this.allOfTheData = res;
+          var rowsThisPage = this.allOfTheData.slice(params.startRow, params.endRow);
+          var lastRow = -1;
+          if (this.allOfTheData.length <= params.endRow) {
+            lastRow = this.allOfTheData.length;
+          }
+          params.successCallback(rowsThisPage, lastRow);
+        })
+      },
+      rowCount: this.allOfTheData.length
+    };
+
+    this.gridOptions.api.setDatasource(dataSource);
+
   }
   public onRowClicked(e) {
     if (e.event.target !== undefined) {
@@ -73,12 +103,8 @@ export class DanhSachNhanVien {
       let actionType = e.event.target.getAttribute("data-action-type");
 
       switch (actionType) {
-        case "view":
-          return this.onActionViewClick(data);
         case "edit":
           return this.onActionEditClick(data);
-        case "remove":
-          return this.onActionRemoveClick(data);
       }
     }
   }
@@ -86,32 +112,7 @@ export class DanhSachNhanVien {
     console.log("View action clicked", data);
   }
 
-  public onActionRemoveClick(data: NhanVien) {
-    swal({
-      title: "Bạn có chắc xóa không",
-      text: "Bạn sẽ không khôi phục lại được nhân viên nếu đã bị xóa",
-      type: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#DD6B55",
-      confirmButtonText: "Có, Xóa",
-      cancelButtonText: "Không, hủy thao tác!",
-      closeOnConfirm: false,
-      closeOnCancel: false
-    },
-      (isConfirm) => {
-        if (isConfirm) {
-          this.quanLyNhanVienService.DeleteNhanVien(data.MaNv).then(res => {
-            swal("Đã xóa!", "bạn đã xóa thành công", "success");
-            this.onReady();
-          }).catch(err => {
-            swal("Lỗi", "Thực hiện không thành công", "error");
-          })
 
-        } else {
-          swal("Đã hủy", "đã hủy thao tác", "error");
-        }
-      })
-  }
   public onActionEditClick(data: NhanVien) {
     console.log("data from mng", data);
     this.dialogService.open({ viewModel: SaveNhanVien, model: new NhanVien(data) }).then((result) => {
@@ -149,31 +150,43 @@ export class DanhSachNhanVien {
       }
     });
   }
-  createNewDatasource() {
-    if (!this.allOfTheData) {
-      return;
-    }
-    var dataSource = {
-      getRows: (params) => {
-        // console.log('asking for ' + params.startRow + ' to ' + params.endRow);
-        // setTimeout(() => {
-        //   console.log("allOfTheDate when create datasource : ", this.allOfTheData);
 
-        // }, 500);
-        this.quanLyNhanVienService.GetNhanViens().then(res => {
-          this.allOfTheData = res;
-          var rowsThisPage = this.allOfTheData.slice(params.startRow, params.endRow);
-          var lastRow = -1;
-          if (this.allOfTheData.length <= params.endRow) {
-            lastRow = this.allOfTheData.length;
-          }
-          params.successCallback(rowsThisPage, lastRow);
-        })
-      },
-      rowCount: this.allOfTheData.length
-    };
+  //ag-grid events
+  onRowDoubleClicked(e) {
+    console.log("event", );
+  }
+  onRowSelected(e) {
+    this.selectdRows = this.gridOptions.api.getSelectedRows().map(x => new NhanVien(x));
+  }
 
-    this.gridOptions.api.setDatasource(dataSource);
+  // view events
+  deleteSelected() {
+    let maNvs = this.selectdRows.map(x => x.MaNv);
+    swal({
+      title: "Bạn có chắc xóa không",
+      text: "Bạn sẽ không khôi phục lại được nhân viên nếu đã bị xóa",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Có, Xóa",
+      cancelButtonText: "Không, hủy thao tác!",
+      closeOnConfirm: false,
+      closeOnCancel: false
+    },
+      (isConfirm) => {
+        if (isConfirm) {
+          this.quanLyNhanVienService.DeleteNhanViens(maNvs)
+            .then(res => {
+              swal("Thành công", "Lưu thành công", "success");
+              this.onReady();
+            }).catch((err) => {
+
+              swal("Không thành công", `${err}`, "error")
+            });
+        } else {
+          swal("Đã hủy", "đã hủy thao tác", "error");
+        }
+      })
 
   }
 }
