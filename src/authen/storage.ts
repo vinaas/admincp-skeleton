079c18a8c7487b915e9816cddfe017a3c11334ef
tungptvn <tungpt.hd@gmail.com>
2017-03-13 20:54:52 +0700

    feat(*) add storage

--- src/authen/storage.ts
+++ src/authen/storage.ts
@@ -1,23 +1,24 @@
-import { inject } from 'aurelia-dependency-injection';
 import {
   PLATFORM
 } from 'aurelia-pal';
 import { Config } from './config'
-@inject(Config)
 export class Storage {
-  constructor(private config: Config) {
+  constructor() {
 
   }
 
   get(key: string): string {
-    return PLATFORM.global[this.config.storage].getItem(key);
+
+    let data = PLATFORM.global['localStorage'].getItem(key);
+    return JSON.parse(data);
   }
 
   set(key: string, value: string) {
-    PLATFORM.global[this.config.storage].setItem(key, value);
+    let data = JSON.stringify(value);
+    PLATFORM.global['localStorage'].setItem(key, data);
   }
 
   remove(key: string) {
-    PLATFORM.global[this.config.storage].removeItem(key);
+    PLATFORM.global['localStorage'].removeItem(key);
   }
 }
