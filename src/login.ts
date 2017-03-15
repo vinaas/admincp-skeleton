76dde899e1a9252d42e73d63212000a72f994eca
tungptvn <tungpt.hd@gmail.com>
2017-03-15 13:32:34 +0700

    chore(*): fix setRoot after authen

--- src/login.ts
+++ src/login.ts
@@ -20,11 +20,15 @@ export class Login {
     if (this.passWord.length == 4) {
       swal({ title: 'Đăng nhập', text: 'Đăng nhập thành công', type: 'success', timer: 1000, showConfirmButton: false });
       this.authSrv.login({ userName: 'tungpt', passWord: 'passs' }).then(result => {
-        
+        this.router.navigate('/');
+        this.aurelia.setRoot('app')
+          .then(() => {
+            this.router.navigate('/');
+          });
       })
     }
-    else{
-      swal({title:"Lỗi", text:"Nhập mật khẩu 4 kí tự để đăng nhập", type:"error", timer:3000, showConfirmButton :false })
+    else {
+      swal({ title: "Lỗi", text: "Nhập mật khẩu 4 kí tự để đăng nhập", type: "error", timer: 3000, showConfirmButton: false })
     }
   }
 }
