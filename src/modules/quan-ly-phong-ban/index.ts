9067af43d57897dd3b601faa3a3896198bc28fa8
tungptvn <tungpt.hd@gmail.com>
2017-03-20 13:24:49 +0700

    feat(nav): update style sub nav

--- src/modules/quan-ly-phong-ban/index.ts
+++ src/modules/quan-ly-phong-ban/index.ts
@@ -1,11 +1,14 @@
 import { inject } from 'aurelia-dependency-injection';
 import { Router, RouterConfiguration } from 'aurelia-router';
 export class QuanLyNhanVien {
-    router: Router;
-    heading = 'Quản lý phòng ban';
-    configureRouter(config: RouterConfiguration, router: Router) {
-        config.map([
-            { route: ['', 'ds-phong-ban'], name: 'ds-phong-ban', moduleId: './ds-phong-ban', nav: true, title: 'Danh sách phòng ban' }]);
-        this.router = router;
-    }
+  router: Router;
+  heading = 'Quản lý phòng ban';
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      { route: ['', 'ds-phong-ban'], name: 'ds-phong-ban', moduleId: './ds-phong-ban', nav: true, title: 'Danh sách phòng ban' },
+      { route: 'other-module', name: 'other-module', moduleId: './other-module', nav: true, title: 'other menu' },
+    ]);
+    this.router = router;
+    console.log("router", this.router);
+  }
 }
