50e3c1d06458e67ef2801ceb7979f86a51207f5c
tungptvn <tungpt.hd@gmail.com>
2017-03-22 10:54:43 +0700

    feat(ui): refact select2 attribute

--- src/components/select2/index.ts
+++ src/components/select2/index.ts
@@ -1,33 +1,28 @@
-import { bindable, inject, customAttribute } from 'aurelia-framework';
-import $ from 'jquery';
+import {
+  bindable,
+  bindingMode,
+  inject,
+  customAttribute
+} from 'aurelia-framework';
+import $ from "jquery";
 import 'select2';
 @inject(Element)
 @customAttribute('select2')
 
 export class Select2CustomAttribute {
-    @bindable otherProp='some data';
-    element: any;
-    value;
-    constructor(element) {
-        this.element = element;
+  element: any;
+  constructor(element) {
+    this.element = element;
+  }
+  attached() {
+    $(this.element).select2()
+      .on('select2:select', (e) => {
+        this.element.dispatchEvent(new Event('change'));
+      })
+  }
+  detached() {
+    $(this.element).select2('destroy');
+  }
 
-    }
-    attached() {
-        $(this.element).select2()
-            .on('select2:select', () => this.element.dispatchEvent(new Event('change')));
-        $(this.element).select2('val', `${this.value}`);
-
-    }
-
-    bind(ct) {
-        console.log(ct);
-    }
-    detached() {
-        $(this.element).select2('destroy');
-    }
-    valueChanged(n, o) {
-        console.log('value', this.value);
-        console.log('value2', this.element.value);
-    }
 
 }
