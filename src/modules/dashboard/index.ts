19733a66c5304b52b52cb0f866d68a192c61ec81
tungptvn <tungpt.hd@gmail.com>
2017-03-22 11:43:12 +0700

    chore(*)

--- src/modules/dashboard/index.ts
+++ src/modules/dashboard/index.ts
@@ -1,20 +1,14 @@
-import { inject } from 'aurelia-dependency-injection';
-import { PLATFORM } from 'aurelia-pal';
+import $ from 'jquery';
+import * as moment from 'moment';
 export class DashBoard {
-    myselection = "AK";
-    /**
-     *
-     */
+  myvl = '2';
 
-    myvl = 2;
-    color = 'green';
-    constructor() {
-        this.myvl = 2;
-    }
-    attached() {
-    }
-    bind(ct) {
-        console.log('parent content', ct);
-    }
+ 
 
+  constructor() {
+    
+  }
+  attached() {
+
+  }
 }
